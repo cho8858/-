1e0f9cfa5f47ffe95fe816b9849ac1866a336d8a
cho8858 <idcara11@naver.com>
2025-09-05 20:42:27 +0900

    [level 0] Title: 편지, Time: 0.19 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        String [] m =message.split("");
+        answer= m.length*2;
+        return answer;
+    }
+}
